9d0cb8442c45568cabd29a8060d849ce5f44047a
Tam Vu <vutam_a@hotmail.com>
2018-12-01 03:32:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else katz_deli.size > 0 
+    line_call = []
+    counter = 1
+    katz_deli.each do |name|
+      puts "#{counter}. #{name}"
+      counter += 1
+    end 
+   
+    #katz_deli.each do |name|
+      #puts "The line is currently: #{counter}. #{name}"
+      #counter += 1 
+     
+  end 
+
+end
