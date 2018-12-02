67c7c1a86b5a2ac2a20509175939b4c3f20cfc9e
Tam Vu <vutam_a@hotmail.com>
2018-12-02 20:35:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else katz_deli.size > 0 
+    line_call = []
+    counter = 1
+    katz_deli.each do |name|
+      line_call << "#{counter}. #{name}"
+      counter += 1
+    end 
+    puts "The line is currently: #{line_call.join(" ")}"
+  end 
+end 
+
+def take_a_number(katz_deli, name)
+  katz_deli << name 
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end 
+
